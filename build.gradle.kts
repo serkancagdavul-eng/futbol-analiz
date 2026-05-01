d72594a920dfc243fa5c57ae49f8bdcff8751d84
Sserkan <serkancagdavul@gmail.com>
2026-05-01 10:48:25 +0300

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// build.gradle.kts (Project)
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
+}
